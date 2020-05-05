74008960178ef68f4d9acdaf52f0da17cf2c2cb4
Davidss0397 <david.silvas@udea.edu.co>
2020-05-04 23:50:21 -0500

    Added Function to read Vertices closes #1

---
+++ Parcial3/INPUT/data_reader.h
@@ -0,0 +1,22 @@
+#ifndef TRIANGLE_READER_H
+#define TRIANGLE_READER_H
+
+//#define FILE="triangles.txt"
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//This function extracts the information from the triangles file
+//it assigns the corresponding x, y values for certain node
+void ExtractInfo(int*, //K
+		 int*, //N
+		 int*, //M
+		 int*, //n
+		 int*, //m
+		 vector<vector <double> > & //vertex
+		 );
+  
+#endif
